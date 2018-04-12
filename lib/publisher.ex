9e0e367c1e5e0f97b8129010500af0fb03943624
Grant McLendon <grant.mclendon@gmail.com>
2018-04-12 10:31:35 +0200

    Remove application/implementation specific metadata schema from Publisher and mock_bus

--- lib/publisher.ex
+++ lib/publisher.ex
@@ -51,91 +51,19 @@ defmodule KaufmannEx.Publisher do
   end
 
   @doc """
-  Replace "command." with "event." in event names
-  """
-  @spec cmd_to_event(atom) :: atom
-  def cmd_to_event(command_name) do
-    command_name
-    |> to_string
-    |> String.replace_prefix("command.", "event.")
-    |> String.to_atom()
-  end
+  Produces message to configured producer
 
-  @doc """
-    Publishes error for a given event
+  Chooses publication topic from Topic Strategy
 
-    prepends the message_name with "event.error"
-  """
-  @spec publish_error(String.t() | atom, any, any, any) :: :ok | {:error, any}
-  def publish_error(event_name, error, _orig_payload, meta \\ %{}) do
-    error_payload = %{
-      error: error
-    }
-
-    publish(:"event.error.#{event_name}", error_payload, meta)
-  end
-
-  @doc """
-    Publishes error for a given event
-
-    prepends the message_name with "event.error"
-  """
-  @spec publish_error(KaufmannEx.Schemas.Event.t(), term) :: :ok | {:error, any}
-  def publish_error(%KaufmannEx.Schemas.Event{} = event, error) do
-    error_payload = %{
-      error: error
-    }
-
-    publish(:"event.error.#{event.name}", error_payload, event.meta)
-  end
-
-  @doc """
-  Injects metadata then calls `produce/2`
-
-  Inserted metadata conforms to the `event_metadata/2`
-
-  Events with Metadata are produced to the Producer set in config `:kaufmann_ex, :producer_mod`. This defaults to `KaufmannEx.Publisher`
+  Events with are produced to the Producer set in config `:kaufmann_ex, :producer_mod`. This defaults to `KaufmannEx.Publisher`
   """
   @spec publish(atom, map, map) :: :ok
-  def publish(event_name, payload, context \\ %{}) do
-    message_body = %{
-      payload: payload,
-      meta: event_metadata(event_name, context)
-    }
-
+  def publish(event_name, message_body, context \\ %{}) do
+    log_time_took(context[:timestamp], event_name)
     {:ok, topic} = choose_topic(event_name, context)
 
     producer = Application.fetch_env!(:kaufmann_ex, :producer_mod)
     producer.produce(topic, event_name, message_body, context)
-  end
-
-  @doc """
-  generate metadata for an event
-
-  ```
-  %{
-      message_id: Nanoid.generate(),
-      emitter_service: KaufmannEx.Config.service_name(), # env var SERVICE_NAME
-      emitter_service_id: KaufmannEx.Config.service_id(), # env var HOST_NAME
-      callback_id: context[:callback_id],
-      message_name: event_name |> to_string,
-      timestamp: DateTime.to_string(DateTime.utc_now())
-    }
-  ```
-  """
-  @spec event_metadata(atom, map) :: map
-  def event_metadata(event_name, context) do
-    log_time_took(context[:timestamp], event_name)
-
-    %{
-      message_id: Nanoid.generate(),
-      emitter_service: KaufmannEx.Config.service_name(),
-      emitter_service_id: KaufmannEx.Config.service_id(),
-      callback_id: context[:callback_id],
-      message_name: event_name |> to_string,
-      timestamp: DateTime.to_string(DateTime.utc_now()),
-      callback_topic: Map.get(context, :next_callback_topic, nil)
-    }
   end
 
   defp choose_partition(topic, metadata) do
