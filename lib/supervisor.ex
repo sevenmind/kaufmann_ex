424fea61ac8b0d50cdcdca0c8cecd3afaeeb27fd
Grant McLendon <grant.mclendon@gmail.com>
2018-05-03 16:53:51 +0200

    Custom supervisor option is uncescessary. Application should just not start KaufmannEx in tests unless desired.

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -14,48 +14,37 @@ defmodule KaufmannEx.Supervisor do
   end
 
   def init(opts \\ []) do
-    children = supervisor_children(opts)
+    consumer_group_name = KaufmannEx.Config.consumer_group()
+    topics = KaufmannEx.Config.default_topics()
+    gen_consumer_mod = KaufmannEx.Config.gen_consumer_mod()
 
-    opts = [strategy: :one_for_one]
-    Supervisor.init(children, opts)
-  end
+    consumer_group_opts = [
+      gen_consumer_mod,
+      consumer_group_name,
+      topics,
+      opts
+    ]
 
-  def supervisor_children(opts \\ []) do
-    case Mix.env() do
-      :test ->
-        []
+    children = [
+      %{
+        id: KafkaEx,
+        start: {KafkaEx, :start, [[], []]}
+      },
+      %{
+        id: KaufmannEx.Stages.Producer,
+        start: {KaufmannEx.Stages.Producer, :start_link, []}
+      },
+      %{
+        id: KaufmannEx.Stages.Consumer,
+        start: {KaufmannEx.Stages.Consumer, :start_link, []}
+      },
+      %{
+        id: KafkaEx.ConsumerGroup,
+        start: {KafkaEx.ConsumerGroup, :start_link, consumer_group_opts},
+        type: :supervisor
+      }
+    ]
 
-      _ ->
-        consumer_group_name = KaufmannEx.Config.consumer_group()
-        topics = KaufmannEx.Config.default_topics()
-        gen_consumer_mod = KaufmannEx.Config.gen_consumer_mod()
-
-        consumer_group_opts = [
-          gen_consumer_mod,
-          consumer_group_name,
-          topics,
-          opts
-        ]
-
-        [
-          %{
-            id: KafkaEx,
-            start: {KafkaEx, :start, [[], []]}
-          },
-          %{
-            id: KaufmannEx.Stages.Producer,
-            start: {KaufmannEx.Stages.Producer, :start_link, []}
-          },
-          %{
-            id: KaufmannEx.Stages.Consumer,
-            start: {KaufmannEx.Stages.Consumer, :start_link, []}
-          },
-          %{
-            id: KafkaEx.ConsumerGroup,
-            start: {KafkaEx.ConsumerGroup, :start_link, consumer_group_opts},
-            type: :supervisor
-          }
-        ]
-    end
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
