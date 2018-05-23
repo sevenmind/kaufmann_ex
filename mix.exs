08871062d553280695c0cbc5e6010d429cd6337a
Grant McLendon <grant.mclendon@gmail.com>
2018-05-23 09:35:08 +0200

    Version Bump 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KaufmannEx.MixProject do
   def project do
     [
       app: :kaufmann_ex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
